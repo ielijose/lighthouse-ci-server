cbd7b0cbebfabc5f38cf605f8bcca87521197f82
Eli José Carrasquero <ielijose@gmail.com>
2020-10-19 17:06:58 -0300

    Initial Server

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const { createServer } = require('@lhci/server');
+
+console.log('Starting server...');
+createServer({
+  port: process.env.PORT,
+  storage: {
+    storageMethod: 'sql',
+    sqlDialect: 'sqlite',
+    sqlDatabasePath: './db.sql',
+  },
+  //   basicAuth: {
+  //     username: 'username',
+  //     password: 'password',
+  //   },
+}).then(({ port }) => console.log('LHCI listening on port', port));
